1332b4729adc79e9c9df5d5864f01cf9700a51e0
tencups <michaelwwang17@gmail.com>
2024-10-24 17:19:01 -0400

    add spreadsheet routing and fix root route to login, add spreadsheet button to dashboard

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,17 +4,18 @@ import "./App.css";
 import ForgotPasswordPage from "./pages/Login/forgot-passsowrd"; // Assuming this is the component for forgot password
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard";
-import Spreadsheet from "./components/Spreadsheet/Spreadsheet";
 import CalendarPage from "./components/Calendar/CalendarPage";
 import UserProfile from "./components/UserProfile/UserProfile";
+import Spreadsheet from "./components/Spreadsheet/Spreadsheet";
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Spreadsheet />} />
+        <Route path="/" element={<Login/>} /> 
         <Route path="/user/:uid" element={<UserProfile />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
+        <Route path = "/spreadsheet" element={<Spreadsheet />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
