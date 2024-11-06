a750d8a8f8ef2b75219c6624dd84c611d42fa13b
RohanBhatnagar <rohan.bha09@gmail.com>
2024-11-05 21:00:16 -0500

    batch dist mat, locally simulating kmedi cluster

---
+++ my-app/functions-python/local_batch_testing.py
@@ -0,0 +1,85 @@
+import googlemaps
+from firebase_functions import https_fn
+from dotenv import load_dotenv
+from k_means_constrained import KMeansConstrained
+from kmedoids import KMedoids
+from pydantic import BaseModel
+from typing import List, Tuple, Any
+import json
+import os
+import numpy as np
+
+
+def test(coords):
+    def chunk_coordinates(coords, chunk_size=10):
+        for i in range(0, len(coords), chunk_size):
+            yield coords[i:i + chunk_size]
+
+    # Create a mock Google Maps Distance Matrix with non-uniform distances. We assign clusters linearly, 
+    # but reassign distances based on cluster calculation to simulate low inter cluster dist and high intra cluster distance
+    def mock_google_maps_distance_matrix(origins, destinations):
+        n = len(coords)
+        num_clusters = 15
+        cluster_size = n // num_clusters
+
+        response = {
+            "rows": []
+        }
+
+        for o in origins:
+            row = {"elements": []}
+            for d in destinations:
+                o_cluster = o // cluster_size
+                d_cluster = d // cluster_size
+
+                if o_cluster == d_cluster:
+                    distance_value = np.random.randint(1, 10)
+                else:
+                    distance_value = np.random.randint(50, 100)
+
+                row["elements"].append({
+                    "status": "OK",
+                    "duration": {"value": distance_value}
+                })
+            response["rows"].append(row)
+
+        return response
+
+    # construct complete distance matrix from a list of coordinates
+    def construct_distance_matrix(coords):
+        n = len(coords)
+
+        distance_matrix = np.zeros((n, n))
+        batches = list(chunk_coordinates(coords, 10))
+
+        # Loop over each pair of origin and destination batches to fill the distance matrix
+        for i, origin_batch in enumerate(batches):
+            for j, destination_batch in enumerate(batches):
+                response = mock_google_maps_distance_matrix(
+                    origin_batch, destination_batch)
+                for origin_index, row in enumerate(response["rows"]):
+                    for destination_index, element in enumerate(row["elements"]):
+                        if element["status"] == "OK":
+                            global_origin_index = i * 10 + origin_index
+                            global_destination_index = j * 10 + destination_index
+                            distance_matrix[global_origin_index][global_destination_index] = element["duration"]["value"]
+        return distance_matrix
+
+    drivers_count = 15
+    min_deliveries = 3
+    max_deliveries = 10
+
+    distance_matrix = construct_distance_matrix(coords)
+
+    print(distance_matrix)
+
+    # metric is the distance itself
+    kmedoids = KMedoids(n_clusters=drivers_count, metric="precomputed", method="fasterpam").fit(
+        distance_matrix
+    )
+    labels = kmedoids.labels_
+    print(labels)
+
+
+coords = [i for i in range(105)]
+test(coords)
--- my-app/functions-python/main.py
+++ my-app/functions-python/main.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 from k_means_constrained import KMeansConstrained
 from kmedoids import KMedoids
 from pydantic import BaseModel
-from typing import List, Tuple
+from typing import List, Tuple, Any
 import json
 import os
+import numpy as np
 
 load_dotenv()  # TODO: Remove once done with dev
 # TODO: Make sure to add the api key in the environment vars in the firebase console
@@ -23,6 +24,16 @@ class ClusterDeliveriesResponse(BaseModel):
     labels: List[int]
 
 
+class OptimalRouteRequest(BaseModel):
+    coords: List[Tuple[float, float]]
+
+# TODO: Determine what we need to return
+
+
+class OptimalRouteResponse(BaseModel):
+    directions: Any
+
+
 # TODO: Implement Authorization if that is configured
 # TODO: Add rate limiting or caching since we are using google maps API
 @https_fn.on_request()
@@ -30,20 +41,58 @@ def cluster_deliveries_k_medioids(req: https_fn.Request) -> https_fn.Response:
     body = ClusterDeliveriesRequest(**req.get_json())
 
     coords = body.coords
+
+    # Returns list of batches
+    def chunk_coordinates(coords, chunk_size=10):
+        for i in range(0, len(coords), chunk_size):
+            yield coords[i:i + chunk_size]
+
+    # TODO: mock gmaps call, remove later.
+    def mock_google_maps_distance_matrix(origins, destinations):
+        response = {
+            "rows": [
+                {"elements": [{"status": "OK", "duration": {
+                    "value": d + o}} for d in destinations]}
+                for o in origins
+            ]
+        }
+        return response
+
+    # construct complete distance matrix from a list of coordinates
+    def construct_distance_matrix(coords):
+        n = len(coords)
+
+        distance_matrix = np.zeros((n, n))
+        batches = list(chunk_coordinates(coords, 10))
+
+        # Loop over each pair of origin and destination batches to fill the distance matrix
+        for i, origin_batch in enumerate(batches):
+            for j, destination_batch in enumerate(batches):
+
+                # TODO: Replace with call to gmaps distance API.
+                response = mock_google_maps_distance_matrix(
+                    origin_batch, destination_batch)
+            
+                # gmaps = googlemaps.Client(key=os.environ["MAPS_API_KEY"])
+                # distance_matrix = gmaps.distance_matrix(
+                #     origins=coords, destinations=coords, mode="driving"
+                # )
+
+                for origin_index, row in enumerate(response["rows"]):
+                    for destination_index, element in enumerate(row["elements"]):
+                        if element["status"] == "OK":
+                            global_origin_index = i * 10 + origin_index
+                            global_destination_index = j * 10 + destination_index
+                            distance_matrix[global_origin_index][global_destination_index] = element["duration"]["value"]
+        return distance_matrix
+
     drivers_count = body.drivers_count
     # TODO: Determine if we nede to enforce min and max constraints here
     min_deliveries = body.min_deliveries
     max_deliveries = body.max_deliveries
 
-    # TODO: Fix such that it can handle for that 10 locations
-    gmaps = googlemaps.Client(key=os.environ["MAPS_API_KEY"])
-    distance_matrix = gmaps.distance_matrix(
-        origins=coords, destinations=coords, mode="driving"
-    )
-    distance_matrix = [
-        [element["duration"]["value"] for element in row["elements"]]
-        for row in distance_matrix["rows"]
-    ]
+    # TODO: Test with actual gmap calls
+    distance_matrix = construct_distance_matrix(coords)
 
     kmedoids = KMedoids(n_clusters=drivers_count, method="fasterpam").fit(
         distance_matrix
@@ -51,7 +100,6 @@ def cluster_deliveries_k_medioids(req: https_fn.Request) -> https_fn.Response:
     labels = kmedoids.labels_
 
     # TODO: Alter groups if we want to handle that in server side
-
     data = ClusterDeliveriesResponse(labels=labels)
     return https_fn.Response(
         response=json.dumps(data.model_dump()),
@@ -79,6 +127,31 @@ def cluster_deliveries_k_means(req: https_fn.Request) -> https_fn.Response:
     labels = kmeans.labels_
 
     data = ClusterDeliveriesResponse(labels=labels)
+
+    return https_fn.Response(
+        response=json.dumps(data.model_dump()),
+        status=201,
+        content_type="application/json",
+    )
+
+@https_fn.on_request()
+def calculate_optimal_cluster_route(req: https_fn.Request) -> https_fn.Response: 
+    body = OptimalRouteRequest(**req.get_json())
+
+    waypoints = [f"{lat},{lng}" for lat, lng in body.coords]
+
+    gmaps = googlemaps.Client(key=os.environ["MAPS_API_KEY"])
+
+    # TODO: Start and destination may be determined by the client. For now pick any waypoint and form a loop
+    directions_result = gmaps.directions(
+        origin=waypoints[0],
+        destination=waypoints[0], 
+        waypoints=waypoints[1:],  
+        optimize_waypoints=True,
+        mode="driving"
+    )
+
+    data = OptimalRouteResponse(directions=directions_result)
 
     return https_fn.Response(
         response=json.dumps(data.model_dump()),
