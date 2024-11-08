f27f0d98b95f7e91c2aa23037d69b375fd935a94
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2024-11-07 22:50:15 -0500

    navbar finished

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,22 +1,55 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import ForgotPasswordPage from "./pages/Login/forgot-passsowrd"; // Assuming this is the component for forgot password
+import ForgotPasswordPage from "./pages/Login/forgot-passsowrd";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard";
 import CalendarPage from "./components/Calendar/CalendarPage";
 import Profile from './pages/Profile/Profile';
 import Spreadsheet from "./components/Spreadsheet/Spreadsheet";
+import BasePage from './pages/Base/Base';
+
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Public routes */}
         <Route path="/" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/spreadsheet" element={<Spreadsheet />} />
+        
+        {/* Protected or main routes wrapped with BasePage */}
+        <Route
+          path="/dashboard"
+          element={
+            <BasePage>
+              <Dashboard />
+            </BasePage>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <BasePage>
+              <CalendarPage />
+            </BasePage>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <BasePage>
+              <Profile />
+            </BasePage>
+          }
+        />
+        <Route
+          path="/spreadsheet"
+          element={
+            <BasePage>
+              <Spreadsheet />
+            </BasePage>
+          }
+        />
       </Routes>
     </Router>
   );
