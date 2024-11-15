cb9f1ce63d0b5543342b1b62063251ee0e9aa4e2
Neel Jay <neeljay03@gmail.com>
2024-11-14 19:07:13 -0500

    Add create functionality in spreedsheet

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useParams,
+} from "react-router-dom";
 import "./App.css";
-import ForgotPasswordPage from "./pages/Login/forgot-passsowrd"; // Assuming this is the component for forgot password
+import ForgotPasswordPage from "./pages/Login/forgot-passsowrd";
 import Login from "./pages/Login/Login";
 import Dashboard from "./pages/Dashboard";
 import CalendarPage from "./components/Calendar/CalendarPage";
 import Profile from "./pages/Profile/Profile";
 import Spreadsheet from "./components/Spreadsheet/Spreadsheet";
+
+const ProfileRouter = () => {
+  const { uid } = useParams() as { uid: string };
+  return <Profile uid={uid} />;
+};
+
 function App() {
   return (
     <Router>
@@ -15,6 +26,7 @@ function App() {
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile/:uid" element={<ProfileRouter />} />
         <Route
           path="/profile"
           element={<Profile uid={"kBTldkURU4udo9kP9Rwt"} />}
