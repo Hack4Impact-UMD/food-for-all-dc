67fad8db89734728630ad7491d71a851cb2a5705
T <23esingh@gmail.com>
2025-04-26 14:58:28 -0400

    functional deletion confirmation

---
+++ my-app/src/components/Spreadsheet/DeleteClientModal.tsx
@@ -0,0 +1,62 @@
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+
+type DeleteClientModalProps = {
+  handleMenuClose: () => void;
+  handleDeleteRow: (id: string) => Promise<void>;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  id: string;
+};
+
+const DeleteClientModal = ({ handleMenuClose, handleDeleteRow, open, setOpen ,id }: DeleteClientModalProps) => {
+
+
+  const handleCloseDeleteConfirm = () => {
+    setOpen(false);    // Close the dialog
+    handleMenuClose();
+  };
+
+  const handleConfirmDelete = () => {
+    if (id) {
+      console.log("DELETED")
+      handleDeleteRow(id); 
+    }
+    handleCloseDeleteConfirm();    
+  };
+
+
+  return (
+    <> 
+      <Dialog
+        open={open}
+        onClose={handleCloseDeleteConfirm} // Close if clicking outside
+        aria-labelledby="delete-confirm-dialog-title"
+        aria-describedby="delete-confirm-dialog-description"
+      >
+        <DialogTitle id="delete-confirm-dialog-title">
+          Confirm Deletion
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="delete-confirm-dialog-description">
+            Are you sure you want to delete this client? This action cannot be undone.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDeleteConfirm} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleConfirmDelete} color="error" autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+export default DeleteClientModal;
