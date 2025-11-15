986dcaa0283468bb5c8b3fa18cde0aa526bd486e
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2025-11-15 16:11:55 -0500

    removal of useless file

--- my-app/src/backend/cloudFunctionsCalls.ts
+++
@@ -1,4 +0,0 @@
-// This file previously contained unused cloud function calls that have been removed.
-// User creation is now handled directly via AuthUserService.createUser() in CreateUserModal.
-
-export {};
