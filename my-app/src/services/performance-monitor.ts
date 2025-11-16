4e97cf3396660bb6305769a5c6effc604bac9c79
Steven Ha <109867418+stevenha75@users.noreply.github.com>
2025-11-15 19:12:32 -0500

    Update performance-monitor.ts

--- my-app/src/services/performance-monitor.ts
+++ my-app/src/services/performance-monitor.ts
@@ -1,5 +1,7 @@
 import { retry } from "../utils/retry";
 import { ServiceError, formatServiceError } from "../utils/serviceError";
+
+// Not used anywhere in UI but measures performance metrics (LCP, FID, CLS, etc.)
 class PerformanceMonitor {
   private static instance: PerformanceMonitor;
   private metrics: Map<string, number[]> = new Map();
